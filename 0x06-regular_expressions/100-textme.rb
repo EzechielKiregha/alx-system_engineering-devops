5c9d2aaf244c915c69bedc9570e8d0afb5bd1db4
EzechielKiregha <ekiregha@gmail.com>
2024-03-01 15:15:01 +0200

    Text Me

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
-# now I will read the log file line by line
-File.foreach('log_file.txt') do |line|
-  # Let's extract the required information using regular expression
-  match_data = line.match(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/)
-  if match_data
-    sender = match_data[1]
-    receiver = match_data[2]
-    flags = match_data[3]
-    puts "#{sender},#{receiver},#{flags}"
-  end
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <log_file>"
+  exit 1
+end
+
+# Let's extract the required information using regular expression
+match_data = ARGV[0].match(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/)
+if match_data
+  sender = match_data[1]
+  receiver = match_data[2]
+  flags = match_data[3]
+  puts "#{sender},#{receiver},#{flags}"
 end
