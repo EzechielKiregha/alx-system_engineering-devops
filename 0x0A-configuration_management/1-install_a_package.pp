aff75a882eee57bc1f6668b905088513b198438d
EzechielKiregha <ekiregha@gmail.com>
2024-04-21 03:06:01 +0200

    puppet packages manager

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
 # This code will install the flask package 2.1.0
-exec { 'install_flask':
-  command => 'pip3 install Flask==2.1.0',
+# Install pip package
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Install Werkzeug 2.1.1 using pip
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.0.2',
   path    => ['/usr/bin', '/bin'],
+  creates => '/usr/local/lib/python3.8/dist-packages/werkzeug', # Adjust the path as per your Python installation
+}
+
+# Install Flask 2.1.0 using pip
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  creates => '/usr/local/lib/python3.8/dist-packages/flask', # Adjust the path as per your Python installation
+  require => Exec['install_werkzeug'], # Ensure Werkzeug is installed first
 }
