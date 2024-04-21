c7204aafa7a61cc1cf148d23416053e971017a46
EzechielKiregha <ekiregha@gmail.com>
2024-04-21 02:10:28 +0200

    creating the puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-#The code will create a file name school inside the /tmp Direstory
+# The code will create a file name school inside the /tmp Directory
 
 file { '/tmp/school':
-  content   =>  'I love Puppet',
-  mode      =>  '0744',
-  owner     =>  'www-data',
-  group     =>  'www-data',
+  content =>  'I love Puppet',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
 }
