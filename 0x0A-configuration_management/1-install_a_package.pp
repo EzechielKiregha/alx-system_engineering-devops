e8101f0012954c7c235aa18982d8f6a34e6baa89
EzechielKiregha <ekiregha@gmail.com>
2024-04-21 02:52:43 +0200

    puppet packages manager

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#This code will install the package puppet-lint
-package { 'flask':
-  ensure   =>  '2.1.0',
-  provider =>  'pip3',
+# This code will install the flask package 2.1.0
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
 }
