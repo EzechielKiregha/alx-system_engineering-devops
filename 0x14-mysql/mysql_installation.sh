c07107bdcbe3a3f092cf43a5e11f516507cca98b
EzechielKiregha <ekiregha@gmail.com>
2024-05-16 20:14:18 +0200

    Mysql installation on both servers web-01 & web-02

--- 0x14-mysql/mysql_installation.sh
+++ 0x14-mysql/mysql_installation.sh
@@ -1,15 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Define MySQL root password
-MYSQL_ROOT_PASSWORD='Ezechiel01@'
+# Stop MySQL service
+sudo systemctl stop mysql
+
+# Uninstall MySQL packages
+sudo apt-get remove --purge -y mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
+
+# Remove MySQL configuration and data files
+sudo rm -rf /etc/mysql /var/lib/mysql
+
+# Remove MySQL dependencies
+sudo apt-get autoremove -y
+sudo apt-get autoclean
+
+echo "MySQL uninstallation completed."
 
 # Update the package list
 sudo apt-get update
 
-# Install MySQL server
-sudo apt-get install -y mysql-server
+# Download MySQL APT repository configuration tool for MySQL 5.7
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
 
-# Secure MySQL installation
+# Install the MySQL APT repository configuration tool
+sudo dpkg -i mysql-apt-config_0.8.10-1_all.deb
+
+# Select the correct version (5.7) 
+# When prompted the bionic will be best choice
+sudo dpkg-reconfigure mysql-apt-config
+
+# Update the package list again
+sudo apt-get update
+
+# Install MySQL server 5.7
+sudo apt-get install -y mysql-server=5.7.*
+
+# Define MySQL root password
+MYSQL_ROOT_PASSWORD='Ezechiel01@'
 sudo mysql_secure_installation <<EOF
 
 n
@@ -37,4 +63,4 @@ sudo systemctl restart mysql
 # Print MySQL version to verify installation
 mysql --version
 
-echo "MySQL installation and initial setup completed."
+echo "MySQL 5.7 installation and initial setup completed."
